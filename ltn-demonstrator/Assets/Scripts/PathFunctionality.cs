4478447bde1fdc55f7ab731ac7a1f1f68416b3f3
Oscar Rilot <83018768+ORilot@users.noreply.github.com>
2023-12-07 14:34:34 +0000

    Update PathFunctionality.cs
    GeneratePath() changes

--- ltn-demonstrator/Assets/Scripts/PathFunctionality.cs
+++ ltn-demonstrator/Assets/Scripts/PathFunctionality.cs
@@ -14,17 +14,18 @@ public class pathFunctionality
         private set { destinationDistance = value; }
     }
 
-
     //public float DestinationDistance { get; set; }
     // This line might be a better getter / setter, not sure 
 
     public void GeneratePath(Building startBuilding, Building terminalBuilding, Traveller traveller)
     {
-        Edge startEdge = startBuilding.getEdgeLocation();
-        Edge endEdge = terminalBuilding.getEdgeLocation();
-        JunctionNode placeholderNode = new JunctionNode();
-
-        // no clue if this is correct
+        Edge startEdge = &startBuilding.getEdgeLocation();
+        Edge endEdge = &terminalBuilding.getEdgeLocation();
+        List<&Edge> generatedPath = new List<&Edge> { startEdge, endEdge };
+        self.path = generatedPath
+        // This code is supposed to create pointers to start and end Edges,
+        // then put them in a list of pointers to edges
+        // then set the sel.path to that path
     }
 
     public void endPath
