72db2f57989bf98c3d646cf821f466d1c49ae922
Scott Deverinne <78443361+scottdd204@users.noreply.github.com>
2023-12-07 11:35:04 +0000

    Update EdgeFunctionality.cs
    Changes based on comments

--- ltn-demonstrator/Assets/Scripts/EdgeFunctionality.cs
+++ ltn-demonstrator/Assets/Scripts/EdgeFunctionality.cs
@@ -6,11 +6,19 @@ public class Edge : MonoBehaviour
 {
     private JunctionNode origin;
     private JunctionNode destination;
+    private float deltaD;
     private float length;
     private readonly float maxVelocity = float.MaxValue;
     private static float h = 1.0f; // Initial value for H
-    private ModesOfTransportEnum modeOfTransport;
+    public ModeOfTransport modeOfTransport;
     private List<Traveller> vehiclesOnRoad = new List<Traveller>();
+
+    // New Edge Constant
+    public static float H
+    {
+        get { return h; }
+        set { h = value; }
+    }
 
     // Origin & Destination Nodes of this edge
     /*
@@ -52,7 +60,73 @@ public class Edge : MonoBehaviour
         }
     }
 
-    // Other attributes and methods...
+    // Public method to calculate deltaD
+    public void CalculateDeltaD()
+    {
+        // Ensure currentEdge is not null
+        if (currentEdge != null)
+        {
+            // Calculate deltaD based on the given formula
+            deltaD = (currentVelocity * (timeStep)) / (currentEdge.length * H);
+
+            // Optional: You can limit deltaD to maxVelocity if needed
+            deltaD = Mathf.Clamp(deltaD, 0f, maxVelocity);
+
+            // Update the positionOnEdge based on the calculated deltaD
+            positionOnEdge += deltaD;
+
+            // If the traveller has moved beyond the current edge, update the current edge and reset positionOnEdge
+            if (positionOnEdge > currentEdge.length)
+            {
+                // Move to the next edge
+                MoveToNextEdge();
+            }
+        }
+    }
+
+    // Public method to calculate deltaD^(-1)
+    public float TranslateFromTrueToDeltaD(float desiredSpeed, float edgeLength)
+    {
+        // Calculate deltaD^(-1) based on the given formula
+        return (desiredSpeed * edgeLength * H) / timeStep; // TimeStep = 1 frame over frames per sec
+    }
+
+    // New public methods for edge space translation
+    public float UnityToEdgeSpace(float d)
+    {
+        if (length != 0)
+        {
+            return d / length;
+        }
+        else
+        {
+            Debug.LogError("Edge length is zero. Cannot perform UnityToEdgeSpace conversion.");
+            return 0;
+        }
+    }
+
+    public float EdgeSpaceToUnity(float d)
+    {
+        if (length != 0)
+        {
+            return d * length;
+        }
+        else
+        {
+            Debug.LogError("Edge length is zero. Cannot perform EdgeSpaceToUnity conversion.");
+            return 0;
+        }
+    }
+
+    public float UnityToReal(float d)
+    {
+        return d * H;
+    }
+
+    public float RealToUnity(float d)
+    {
+        return d / H;
+    }
 
     // Public behavior
     public void Subscribe(Traveller traveller)
@@ -97,6 +171,89 @@ public class Edge : MonoBehaviour
         {
             // Handle the case where either origin or destination is not assigned
             Debug.LogError("Origin or Destination is not assigned.");
+        }
+    }
+
+    public enum ModeOfTransport
+    {
+        Pedestrian,
+        Bicycle,
+        Car,
+        Van,
+        Truck,
+        Bus
+    }
+
+    public class ModeOfTransportEnum
+    {
+        private ModeOfTransport mode;
+
+        public ModeOfTransportEnum(ModeOfTransport mode)
+        {
+            this.mode = mode;
+        }
+
+        public float MaxVelocity()
+        {
+            switch (mode)
+            {
+                case ModeOfTransport.Pedestrian:
+                    return 5.0f;
+                case ModeOfTransport.Bicycle:
+                    return 15.0f;
+                case ModeOfTransport.Car:
+                    return 120.0f;
+                case ModeOfTransport.Van:
+                    return 80.0f;
+                case ModeOfTransport.Truck:
+                    return 60.0f;
+                case ModeOfTransport.Bus:
+                    return 50.0f;
+                default:
+                    return 0.0f;
+            }
+        }
+
+        public int MaxPassengers()
+        {
+            switch (mode)
+            {
+                case ModeOfTransport.Pedestrian:
+                    return 1;
+                case ModeOfTransport.Bicycle:
+                    return 1;
+                case ModeOfTransport.Car:
+                    return 5;
+                case ModeOfTransport.Van:
+                    return 8;
+                case ModeOfTransport.Truck:
+                    return 2;
+                case ModeOfTransport.Bus:
+                    return 30;
+                default:
+                    return 0;
+            }
+        }
+
+        public int WeightClass()
+        {
+            switch (mode)
+            {
+                case ModeOfTransport.Pedestrian:
+                    return 1;
+                case ModeOfTransport.Bicycle:
+                    return 1;
+                case ModeOfTransport.Car:
+                    return 5;
+                case ModeOfTransport.Van:
+                    return 8;
+                case ModeOfTransport.Truck:
+                    return 2;
+                case ModeOfTransport.Bus:
+                    return 30;
+                default:
+                    return 0;
+            }
         }
     }
 }
