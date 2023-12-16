00247806174e082e06fb87b9c868a2f27578be3c
scottdd204 <sdd204@exeter.ac.uk>
2023-12-16 14:14:52 +0000

    done

---
+++ ltn-demonstrator/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//delete later
+Gizmos Gizmos
+public class Waypoints: MonoBehaviour
+{
+    [Range(0f, 2f)]
+    [SerializedField] private float waypointSize = 1f;
+    private void OnDrawGizmos()
+    // Draws entire path between nodes
+    {
+        // Draw Blue Sphere for all Waypoints
+        foreach(Transform t in transform)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(t.position, waypointSize);
+        }
+        Gizmos.color = Color.red
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position)
+        }
+
+        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+    }
+
+    public Transform GetNextWaypoint(Transform currentWaypoint)
+    {
+        if (currentWaypoint == null)
+        {
+            return transform.GetChild(0);
+        }
+        return null
+    }
+}
