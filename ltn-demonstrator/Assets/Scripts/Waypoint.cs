059676e0df0aca3b5782daadc8370079cffef80d
scottdd204 <sdd204@exeter.ac.uk>
2023-12-16 16:18:57 +0000

    now with a path list

--- ltn-demonstrator/Assets/Scripts/Waypoint.cs
+++ ltn-demonstrator/Assets/Scripts/Waypoint.cs
@@ -2,44 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//delete later
-Gizmos Gizmos
-public class Waypoints: MonoBehaviour
+public class Waypoints : MonoBehaviour
 {
     [Range(0f, 2f)]
-    [SerializedField] private float waypointSize = 1f;
+    [SerializeField] private float waypointSize = 1f;
+
+    // List of valid waypoints
+    [SerializeField] private List<Transform> validWaypoints = new List<Transform>();
+
     private void OnDrawGizmos()
-    // Draws entire path between nodes
     {
         // Draw Blue Sphere for all Waypoints
-        foreach(Transform t in transform)
+        foreach (Transform t in transform)
         {
-            Gizmos.color = Color.blue;
+            Gizmos.color = validWaypoints.Contains(t) ? Color.blue : Color.yellow;
             Gizmos.DrawWireSphere(t.position, waypointSize);
         }
-        Gizmos.color = Color.red
-        for (int i = 0; i < transform.childCount - 1; i++)
-        {
-            Gizmos.DrawLine(transform.GetChild(i).position, transform.GetChild(i + 1).position)
-        }
 
-        Gizmos.DrawLine(transform.GetChild(transform.childCount - 1).position, transform.GetChild(0).position);
+        // Draw connections between valid waypoints (in blue) and all other waypoints (in yellow)
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform currentWaypoint = transform.GetChild(i);
+            Gizmos.color = validWaypoints.Contains(currentWaypoint) ? Color.blue : Color.yellow;
+
+            for (int j = 0; j < transform.childCount; j++)
+            {
+                Transform otherWaypoint = transform.GetChild(j);
+
+                // Draw a line if the current and other waypoints are both valid or both invalid
+                if (validWaypoints.Contains(currentWaypoint) == validWaypoints.Contains(otherWaypoint))
+                {
+                    Gizmos.DrawLine(currentWaypoint.position, otherWaypoint.position);
+                }
+            }
+        }
     }
 
     public Transform GetNextWaypoint(Transform currentWaypoint)
     {
-        if (currentWaypoint == null)
+        if (validWaypoints.Count == 0 || currentWaypoint == null)
         {
-            return transform.GetChild(0);
-        }
-        if (currentWaypoint.GetSiblingIndex() < transform.childCount - 1)
-        {
-            return transform.GetChild(currentWaypoint.GetSiblingIndex() + 1)
-        }
-        else
-        {
-            return transform.GetChild(0);
+            Debug.LogWarning("No valid waypoints defined or current waypoint is null.");
+            return null;
         }
 
+        int index = validWaypoints.IndexOf(currentWaypoint);
+
+        if (index != -1)
+        {
+            // Get the next waypoint in the valid waypoints list
+            int nextIndex = (index + 1) % validWaypoints.Count;
+            return validWaypoints[nextIndex];
+        }
+
+        Debug.LogWarning("Current waypoint not found in the list of valid waypoints.");
+        return null;
     }
 }
