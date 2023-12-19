91aa39ac8a8990ac8070d442c49e28940642579d
scottdd204 <sdd204@exeter.ac.uk>
2023-12-19 14:40:15 +0000

    Directions stated on single connection

--- ltn-demonstrator/Assets/Editor/EdgeLoader.cs
+++ ltn-demonstrator/Assets/Editor/EdgeLoader.cs
@@ -9,25 +9,35 @@ public class EdgeLoader
     public static void LoadEdges()
     {
         Graph graph = GameObject.Find("Graph").GetComponent<Graph>();
-        graph.edges = new List<Edge>();
-        
+
         if (graph == null)
         {
             Debug.LogError("No graph found in scene.");
             return;
         }
+
+        graph.edges = new List<Edge>();
         Waypoint[] waypoints = Object.FindObjectsOfType<Waypoint>();
 
         foreach (Waypoint waypoint in waypoints)
         {
+            // Load bidirectional connections
             foreach (Waypoint adjacentWaypoint in waypoint.adjacentWaypoints)
             {
                 float distance = graph.CalculateDistance(waypoint, adjacentWaypoint);
-                // instantiate edge as empty game object
                 Edge edge = new Edge(waypoint, adjacentWaypoint);
                 graph.edges.Add(edge);
             }
+
+            // Load single connections
+            foreach (Waypoint singleConnectionWaypoint in waypoint.singleConnectionWaypoints)
+            {
+                float distance = graph.CalculateDistance(waypoint, singleConnectionWaypoint);
+                Edge edge = new Edge(waypoint, singleConnectionWaypoint);
+                graph.edges.Add(edge);
+            }
         }
+
         Debug.Log("Calculated " + graph.edges.Count + " edges.");
     }
 }
