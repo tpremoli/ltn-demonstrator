5d5448f3e11cafc72cd307be64eab9eb932ececc
scottdd204 <sdd204@exeter.ac.uk>
2023-12-28 16:24:55 +0000

    Game Manager

---
+++ ltn-demonstrator/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isPaused = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for user input to unpause the game
+        if (isPaused && Input.GetKeyDown(KeyCode.Space))
+        {
+            UnpauseGame();
+        }
+    }
+
+    // Implement a method to pause the game
+    void PauseGame()
+    {
+        Time.timeScale = 0f; // Set time scale to 0 to pause the game
+        isPaused = true;
+    }
+
+    // Implement a method to unpause the game
+    void UnpauseGame()
+    {
+        Time.timeScale = 1f; // Set time scale to 1 to resume the game
+        isPaused = false;
+    }
+
+    // Implement any other game management methods as needed
+}
