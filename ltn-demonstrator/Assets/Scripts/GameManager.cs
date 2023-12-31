d12a1f1a7a5e3b6c0510a59b2cbe777330c2edf7
scottdd204 <sdd204@exeter.ac.uk>
2023-12-31 09:39:35 +0000

    working on pausing from start, removed camera

--- ltn-demonstrator/Assets/Scripts/GameManager.cs
+++ ltn-demonstrator/Assets/Scripts/GameManager.cs
@@ -7,8 +7,10 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PauseGame();
+        isPaused = true;
+        Time.timeScale = 0f; // Game starts paused
     }
+
 
     // Update is called once per frame
     void Update()
