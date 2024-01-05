cbf88f51861165a44ac2097ab8606dae107205af
scottdd204 <sdd204@exeter.ac.uk>
2024-01-05 12:14:30 +0000

    Attempting to fix drag and drop problem

---
+++ ltn-demonstrator/Assets/DragAndDrop2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class Manager : MonoBehaviour
+{
+    [SerializedField]
+    private InputAction onMouseClick;
+
+    
+    
+}
+
