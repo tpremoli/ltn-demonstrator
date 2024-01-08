bfc019fb17ae8a9679b2038b2cae9aee0d383487
scottdd204 <sdd204@exeter.ac.uk>
2024-01-08 12:10:59 +0000

    Slider Graphics Complete

---
+++ ltn-demonstrator/Assets/Scripts/UISliderController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UISliderController : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI sliderValueText;
+
+    [SerializeField]
+    private float maxSliderValue = 100f;
+
+    public void OnSliderValueChanged(float value)
+    {
+        float localValue = value * maxSliderValue;
+        sliderValueText.text = localValue.ToString();
+    }
+
+    
+}
