f095668c749b6b4f51788f08b3af0789c6539413
tpremoli <tomas@premoli.org>
2024-01-13 20:13:45 +0000

    added distnace option for positionBuildings

--- ltn-demonstrator/Assets/Editor/PositionBuildings.cs
+++ ltn-demonstrator/Assets/Editor/PositionBuildings.cs
@@ -2,10 +2,9 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
 
-public class PositionBuildings
+public class PositionBuildings : EditorWindow
 {
-
-    private static readonly float buildingOffset = 10f; // Set this to your desired offset distance
+    private static float buildingOffset = 10f; // Set this to your desired offset distance
 
     [MenuItem("Tools/Buildings/Rotate Buildings to Face Roads")]
     public static void RotateBuildings()
@@ -23,12 +22,30 @@ public class PositionBuildings
                 building.transform.rotation = Quaternion.LookRotation(directionToClosestPoint.normalized, Vector3.up);
             }
         }
+
+        // mark scene as dirty so it saves
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
     }
 
     [MenuItem("Tools/Buildings/Position Buildings on Roads")]
-    public static void PositionBuildingsOnRoads()
+    public static void ShowWindow()
     {
-        float offsetDistance = 4.9f;
+        GetWindow<PositionBuildings>("Position Buildings");
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Configure Building Position", EditorStyles.boldLabel);
+        buildingOffset = EditorGUILayout.FloatField("Offset Distance", buildingOffset);
+
+        if (GUILayout.Button("Position Buildings"))
+        {
+            PositionBuildingsOnRoads(buildingOffset);
+        }
+    }
+
+    public static void PositionBuildingsOnRoads(float offset)
+    {
         List<Building> buildings = new List<Building>(Object.FindObjectsOfType<Building>());
 
         foreach (Building building in buildings)
@@ -42,11 +59,14 @@ public class PositionBuildings
                 Vector3 normalizedDirection = directionFromClosestPointToBuilding.normalized;
 
                 // Calculate the new position by moving "offsetDistance" units towards the building from the closest point
-                Vector3 newPosition = closestPoint + normalizedDirection * offsetDistance;
+                Vector3 newPosition = closestPoint + normalizedDirection * buildingOffset;
 
                 // Set the building's position to this new position
                 building.transform.position = newPosition;
             }
         }
+
+        // mark scene as dirty so it saves
+        UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene());
     }
 }
